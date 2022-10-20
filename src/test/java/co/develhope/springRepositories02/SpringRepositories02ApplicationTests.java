680a2db5e2d41e3d11adbd6d869eb36cf2fd48cb
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-20 11:39:12 +0200

    initial commit

---
+++ src/test/java/co/develhope/springRepositories02/SpringRepositories02ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.springRepositories02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepositories02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
